8abec367c18af2440545ed8e620835258508fcd6
Nayan Chauveau <nayan.chauveau@gmail.com>
2023-04-11 13:35:59 +0200

    feat(lsp): Add eslint + prettier

--- lua/user/lsp/null-ls.lua
+++ lua/user/lsp/null-ls.lua
@@ -12,16 +12,16 @@ local diagnostics = null_ls.builtins.diagnostics
 null_ls.setup {
   debug = false,
   sources = {
-    formatting.prettier.with {
-      extra_filetypes = { "toml" },
-      extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" },
-    },
+    -- formatting.prettier.with {
+    --   extra_filetypes = { "toml" },
+    --   extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" },
+    -- },
     formatting.black.with { extra_args = { "--fast" } },
     formatting.stylua,
     formatting.google_java_format,
     diagnostics.flake8,
-    diagnostics.eslint_d,
-    formatting.eslint_d,
+    diagnostics.eslint,
+    formatting.eslint,
   },
 }
 
